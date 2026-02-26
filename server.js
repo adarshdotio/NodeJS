6c46fe9459ecb96e756267159ed88c25c96f27cb
adarshdotio <urstrulyadxrsh@gmail.com>
2026-02-26 22:54:43 +0530

    update

--- server.js
+++ server.js
@@ -1,34 +1,17 @@
 const express = require('express');
-const https = require('https');
 const http = require('http');
+const path = require('path');
 const fs = require('fs');
 
 const app = express();
-
-// Middleware to redirect HTTP to HTTPS
-app.use((req, res, next) => {
-    if (!req.secure) {
-        return res.redirect(`https://${req.headers.host}${req.url}`);
-    }
-    next();
-});
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Success! Secure Express in Termux!</h1>');
-});
-
-// SSL Options
-const options = {
-    key: fs.readFileSync('./localhost-key.pem'),
-    cert: fs.readFileSync('./localhost.pem')
-};
-
-// Start HTTPS Server
-https.createServer(options, app).listen(3000, () => {
-    console.log('HTTPS Server running on port 3000');
+  res.send('<h1>Success! Express in Termux!</h1>');
 });
 
 // Start HTTP Server for Redirection
-http.createServer(app).listen(5000, () => {
-    console.log('HTTP Redirect Server running on port 5000');
+http.createServer(app).listen(3000, () => {
+    console.log('HTTP Server running on port 5000');
 });
